69ab655ef2a4cd2fa9b1cc037616d14faa02950c
Kim Larsen <kimlarsen@Kim-Larsens-2015-MacBook-Pro.local>
2016-04-10 19:23:24 -0700

    cleaning up helper functions

--- HelperFunctions.R
+++ HelperFunctions.R
@@ -59,32 +59,6 @@ CreateGAMFormula <- function(data, y, s=0.6, type="regspline"){
 }
 
 
-#CreateGAMFormula <- function(data, y, span){
- # names <- names(data[,!(names(data) %in% y)])
- # if (length(names)>0){
- #     for (i in 1:length(names)){
- #       if (i==1){
- #          if (is.factor(data[[names[i]]]) | is.character(data[[names[i]]])){
- #            Formula <- paste0(y," ~", names[i])     
- #          } else if (is.binary(data[[names[i]]]) | length(unique(data[[names[i]]]))<4){
- #             Formula <- paste0(y," ~", names[i])     
- #          } else{
- #             Formula <- paste0(y," ~ lo(", names[i],",span=", span, ")")                 
- #          }
- #        } else{
- #          if (is.factor(data[[names[i]]]) | is.character(data[[names[i]]])){
- #             Formula <- paste0(Formula, "+ ",names[i])
- #          } else if (is.binary(data[[names[i]]]) | length(unique(data[[names[i]]]))<4){
- #             Formula <- paste0(Formula, "+ ",names[i])
- #          } else{
- #             Formula <- paste0(Formula, "+ lo(",names[i],",span=",span,")")  
- #          }
- #       }
- #     }
- #  } 
- #  return(as.formula(Formula))
-#}
-
 # creates dummies to indicate missing values
 CreateMissingDummies <- function(data){
   names <- names(data[,sapply(data, is.numeric)])
